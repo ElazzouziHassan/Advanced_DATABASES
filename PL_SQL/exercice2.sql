6076353666658f589365cd5882a03739c36d77e7
wizardy <wizardhassan99@gmail.com>
2024-01-09 20:40:23 +0100

    commit

--- PL_SQL/exercice2.sql
+++ PL_SQL/exercice2.sql
@@ -1,5 +1,11 @@
--- Creating table Emp10
-CREATE TABLE Emp10 AS SELECT * FROM scott.emp WHERE 1 = 0;
+CREATE TABLE Emp10 AS SELECT * FROM scott.emp 
+  WHERE 1 = 0;
 
--- Creating table Emp_autre
-CREATE TABLE Emp_autre AS SELECT * FROM scott.emp WHERE 1 = 0;
+CREATE TABLE Emp_autre AS SELECT * FROM scott.emp 
+  WHERE 1 = 0;
+
+INSERT INTO Emp10 SELECT * FROM scott.emp
+  WHERE deptno = 10;
+
+INSERT INTO Emp_autre SELECT * FROM scott.emp
+  WHERE deptno <> 10;
