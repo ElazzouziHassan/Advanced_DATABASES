056d47fd8c94f5b826ddeacf47b6686e34ab2894
wizardy <wizardhassan99@gmail.com>
2024-02-03 22:17:54 +0100

    commit

---
+++ PL_SQL/3_Control Flow Statements/db.sql
@@ -0,0 +1,18 @@
+-- if statement :
+IF condition THEN
+   -- Code block
+ELSIF condition THEN
+   -- Code block
+ELSE
+   -- Code block
+END IF;
+
+-- while statement :
+WHILE condition LOOP
+   -- Code block
+END LOOP;
+
+-- for statement :
+FOR i IN 1..10 LOOP
+   -- Code block
+END LOOP;
