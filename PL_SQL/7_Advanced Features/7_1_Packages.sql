be4619c340b79509665daa5e58e09242bb946bd5
wizardy <wizardhassan99@gmail.com>
2024-02-03 22:18:30 +0100

    commit

---
+++ PL_SQL/7_Advanced Features/7_1_Packages.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PACKAGE my_package IS
+   PROCEDURE my_procedure;
+END my_package;
+
+CREATE OR REPLACE PACKAGE BODY my_package IS
+   PROCEDURE my_procedure IS
+      -- Procedure logic
+   BEGIN
+      -- Implementation
+   END my_procedure;
+END my_package;
